595f6c9793f6599da209638decac6bbc7525c126
Niklaus Hirt <nikh@ch.ibm.com>
2023-09-12 13:11:05 +0200

    Training

--- tools/11_practicum/stream_remote_logs.sh
+++ tools/11_practicum/stream_remote_logs.sh
@@ -1,3 +1,3 @@
 
-INSTALL_POD=$(oc get po -n ibm-aiops-installer|grep install|awk '{print$1}')
+INSTALL_POD=$(oc get po -n ibm-aiops-installer|grep ibm-aiops-install-training-step2|awk '{print$1}')
 oc logs -n ibm-aiops-installer -f $INSTALL_POD
