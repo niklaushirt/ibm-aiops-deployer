1b15d3073fa6e7adde7814812285ec3ce5f19acf
Niklaus Hirt <nikh@ch.ibm.com>
2024-03-06 07:59:32 +0100

    LAGS

---
+++ tools/01_demo/inject-gs-anomaly-kafka.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+echo "***************************************************************************************************************************************************"
+echo "***************************************************************************************************************************************************"
+echo ""
+echo ""
+echo ""
+echo "         ________  __  ___     ___    ________       "     
+echo "        /  _/ __ )/  |/  /    /   |  /  _/ __ \____  _____"
+echo "        / // __  / /|_/ /    / /| |  / // / / / __ \/ ___/"
+echo "      _/ // /_/ / /  / /    / ___ |_/ // /_/ / /_/ (__  ) "
+echo "     /___/_____/_/  /_/    /_/  |_/___/\____/ .___/____/  "
+echo "                                           /_/            "
+echo ""
+echo ""
+echo ""
+echo "***************************************************************************************************************************************************"
+echo "***************************************************************************************************************************************************"
+echo ""
+echo " 🚀  IBMAIOPS Inject Logs through Kafka"
+echo ""
+echo "***************************************************************************************************************************************************"
+echo "***************************************************************************************************************************************************"
+
+
+
+export LOG_TYPE=lags   # humio, elk, splunk, ...
+export INDEX_TYPE=lags
+export LOGS_SKEW="-60M"
+
+
+
+
+echo "   ------------------------------------------------------------------------------------------------------------------------------"
+echo "    🔬 Getting Installation Namespace"
+echo "   ------------------------------------------------------------------------------------------------------------------------------"
+
+export AIOPS_NAMESPACE=$(oc get po -A|grep aiops-orchestrator-controller |awk '{print$1}')
+echo "       ✅ OK - IBMAIOps:    $AIOPS_NAMESPACE"
+
+echo " "
+echo "   ------------------------------------------------------------------------------------------------------------------------------"
+echo "   🚀  Initializing..."
+echo "   ------------------------------------------------------------------------------------------------------------------------------"
+
+echo "     📥 Get Working Directories"
+export WORKING_DIR_LOGS="./tools/01_demo/INCIDENT_FILES/robot-shop/$LOG_TYPE"
+echo $WORKING_DIR_LOGS
+
+echo "     📥 Get Kafka Topics"
+export KAFKA_TOPIC_LOGS=$(oc get kafkatopics -n $AIOPS_NAMESPACE | grep cp4waiops-cartridge-logs-elk| awk '{print $1;}')
+
+if [[ "${KAFKA_TOPIC_LOGS}" == "" ]]; then
+    echo "          ❗ Please define a Kafka connection in IBMAIOps of type $LOG_TYPE."
+    echo "          ❗ Existing Log Topics are:"
+    oc get kafkatopics -n $AIOPS_NAMESPACE | grep cp4waiops-cartridge-logs-| awk '{print $1;}'| sed 's/^/                /'
+    echo ""
+    echo "          ❌ Exiting....."
+    #exit 1 
+
+else
+    echo "        🟢 OK"
+fi
+
+
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+if [ "${OS}" == "darwin" ]; then
+      # Suppose we're on Mac
+      export DATE_FORMAT_LOGS="-v$LOGS_SKEW +%Y-%m-%dT%H:%M:%S.000000+00:00"
+      #export DATE_FORMAT_LOGS="-v$LOGS_SKEW +%Y-%m-%dT%H:%M:%S.000000+00:00"
+      # HUMIO export DATE_FORMAT_LOGS="+%s000"
+else
+      # Suppose we're on a Linux flavour
+      export DATE_FORMAT_LOGS="-d$LOGS_SKEW +%Y-%m-%dT%H:%M:%S.000000+00:00"
+      #export DATE_FORMAT_LOGS="-d$LOGS_SKEW +%Y-%m-%dT%H:%M:%S.000000+00:00" 
+      # HUMIO export DATE_FORMAT_LOGS="+%s000"
+fi
+
+echo " "
+
+
+#------------------------------------------------------------------------------------------------------------------------------------
+#  Get Kafkacat executable
+#------------------------------------------------------------------------------------------------------------------------------------
+echo "     📥  Getting Kafkacat executable"
+if [ -x "$(command -v kafkacat)" ]; then
+      export KAFKACAT_EXE=kafkacat
+else
+      if [ -x "$(command -v kcat)" ]; then
+            export KAFKACAT_EXE=kcat
+      else
+            echo "     ❗ ERROR: kafkacat is not installed."
+            echo "     ❌ Aborting..."
+            exit 1
+      fi
+fi
+echo " "
+
+
+echo "     🔐 Get Kafka Password"
+export KAFKA_SECRET=$(oc get secret -n $AIOPS_NAMESPACE |grep 'aiops-kafka-secret'|awk '{print$1}')
+export SASL_USER=$(oc get secret $KAFKA_SECRET -n $AIOPS_NAMESPACE --template={{.data.username}} | base64 --decode)
+export SASL_PASSWORD=$(oc get secret $KAFKA_SECRET -n $AIOPS_NAMESPACE --template={{.data.password}} | base64 --decode)
+export KAFKA_BROKER=$(oc get routes iaf-system-kafka-0 -n $AIOPS_NAMESPACE -o=jsonpath='{.status.ingress[0].host}{"\n"}'):443
+
+
+#------------------------------------------------------------------------------------------------------------------------------------
+#  Get the cert for kafkacat
+#------------------------------------------------------------------------------------------------------------------------------------
+echo "     🥇 Getting Kafka Cert"
+oc extract secret/kafka-secrets -n $AIOPS_NAMESPACE --keys=ca.crt --confirm| sed 's/^/            /'
+echo ""
+echo ""
+
+
+
+
+echo "   ----------------------------------------------------------------------------------------------------------------------------------------"
+echo "     🔎  Parameters for Incident Simulation for $APP_NAME"
+echo "   ----------------------------------------------------------------------------------------------------------------------------------------"
+echo "     "
+echo "       🗂  Log Topic                   : $KAFKA_TOPIC_LOGS"
+echo "       🌏 Kafka Broker URL            : $KAFKA_BROKER"
+echo "       🔐 Kafka User                  : $SASL_USER"
+echo "       🔐 Kafka Password              : $SASL_PASSWORD"
+echo "       🖥️  Kafka Executable            : $KAFKACAT_EXE"
+echo "     "
+echo "       📝 Log Type                    : $LOG_TYPE"
+echo "     "
+echo "       📂 Directory for Logs          : $WORKING_DIR_LOGS"
+echo "   ----------------------------------------------------------------------------------------------------------------------------------------"
+echo "   "
+echo "   "
+echo "   ----------------------------------------------------------------------------------------------------------------------------------------"
+echo "     🗄️  Log Files to be loaded"
+echo "   ----------------------------------------------------------------------------------------------------------------------------------------"
+ls -1 $WORKING_DIR_LOGS | grep "json"| sed 's/^/          /'
+echo "     "
+
+
+
+
+
+echo "   "
+echo "      ----------------------------------------------------------------------------------------------------------------------------------------"
+echo "       🚀  Inject Log Files"
+echo "      ----------------------------------------------------------------------------------------------------------------------------------------"
+
+#------------------------------------------------------------------------------------------------------------------------------------
+#  Inject the Data
+#------------------------------------------------------------------------------------------------------------------------------------
+echo "***************************************************************************************************************************************************"
+echo "***************************************************************************************************************************************************"
+echo ""
+echo " 🚀  Simulation for $APP_NAME running..."
+echo "     Stop with CTRL-C"
+echo ""
+echo "***************************************************************************************************************************************************"
+echo "***************************************************************************************************************************************************"
+ACT_COUNT=0
+
+for FILE in $WORKING_DIR_LOGS/*.json; do 
+      #echo $FILE
+
+      mkdir /tmp/lags-logs/ >/tmp/demo.log 2>&1  || true
+      rm /tmp/lags-logs/*.json  >/tmp/demo.log 2>&1  || true
+      rm /tmp/lags-logs/timestampedErrorFile.json    >/tmp/demo.log 2>&1  || true
+      rm /tmp/lags-logs/timestampedErrorFile.json-e    >/tmp/demo.log 2>&1  || true
+      cp $FILE /tmp/lags-logs/timestampedErrorFile.json  >/tmp/demo.log 2>&1  || true
+      #cd /tmp/lags-logs/
+
+      export my_timestamp=$(date $DATE_FORMAT_LOGS)
+      echo "       LOG Base Date: $my_timestamp"
+      sed -i -e "s/MY_TIMESTAMP/$my_timestamp/g" /tmp/lags-logs/timestampedErrorFile.json
+
+      ACT_COUNT=`expr $ACT_COUNT + 1`
+      echo "          Injecting file ($ACT_COUNT/$(($NUM_FILES-1))) - $FILE - $my_timestamp"
+      #echo "                 ${KAFKACAT_EXE} -v -X security.protocol=SASL_SSL -X ssl.ca.location=./ca.crt -X sasl.mechanisms=SCRAM-SHA-512  -X sasl.username=token -X sasl.password=$KAFKA_PASSWORD -b $KAFKA_BROKER -P -t $KAFKA_TOPIC_LOGS -l $FILE   "
+      ${KAFKACAT_EXE} -v -X security.protocol=SASL_SSL -X ssl.ca.location=./ca.crt -X sasl.mechanisms=SCRAM-SHA-512  -X sasl.username=$SASL_USER -X sasl.password=$SASL_PASSWORD -b $KAFKA_BROKER -P -t $KAFKA_TOPIC_LOGS -l /tmp/lags-logs/timestampedErrorFile.json || true 
+done
+
+
+
