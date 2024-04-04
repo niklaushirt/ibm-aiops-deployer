a1caf7a0670fc5d8cb1323804cd1128f15c92dba
Niklaus Hirt <nikh@ch.ibm.com>
2024-04-04 16:09:46 +0200

    cleanup

--- tools/97_addons/experimental/rook-ceph/install-rook-ceph.sh
+++
@@ -1,9 +0,0 @@
-  oc apply -f ./tools/97_addons/experimental/rook-ceph/crds.yaml
-  oc apply -f ./tools/97_addons/experimental/rook-ceph/common.yaml
-  oc apply -f ./tools/97_addons/experimental/rook-ceph/operator-openshift.yaml
-  oc apply -f ./tools/97_addons/experimental/rook-ceph/cluster-test.yaml
-  #oc apply -f ./tools/97_addons/experimental/rook-ceph/storageclass-block.yaml
-  oc patch storageclass rook-cephfs -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
-  oc apply -f ./tools/97_addons/experimental/rook-ceph/filesystem.yaml
-  oc apply -f ./tools/97_addons/experimental/rook-ceph/storageclass-fs.yaml
-  oc apply -f ./tools/97_addons/experimental/rook-ceph/cluster.yaml
