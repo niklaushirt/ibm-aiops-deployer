5e35600a9acdc36d184c579eebd92b4c82b61a51
Niklaus Hirt <nikh@ch.ibm.com>
2024-06-10 11:42:47 +0200

    Simulate Composites

--- tools/97_addons/ibm-aiops-custom-scenarios/synthetic-change/1_simulate-change.sh
+++ tools/97_addons/ibm-aiops-custom-scenarios/synthetic-change/1_simulate-change.sh
@@ -158,7 +158,7 @@ do
     do
         export CUSTOM_PROPERTY_RESOURCE_NAME=$(echo $RESOURCE| cut -d ":" -f 1)
         export CUSTOM_PROPERTY_RESOURCE_TYPE=$(echo $RESOURCE| cut -d ":" -f 2)
-        export OBJ_ID=$(curl -k -s -X "GET" "$TOPO_MGT_ROUTE/1.0/topology/resources?_filter=name=$CUSTOM_PROPERTY_RESOURCE_NAME&_filter=entityTypes=$CUSTOM_PROPERTY_RESOURCE_TYPE&_field=uniqueId&_include_global_resources=false&_include_count=false&_include_status=false&_include_status_severity=false&_include_metadata=false&_return_composites=false" --insecure -u $LOGIN -H 'Content-Type: application/json' -H 'X-TenantID: cfd95b7e-3bc7-4006-a4a8-a73a79c71255'|jq -r -c '._items[]|._id'| tail -1)
+        export OBJ_ID=$(curl -k -s -X "GET" "$TOPO_MGT_ROUTE/1.0/topology/resources?_filter=name=$CUSTOM_PROPERTY_RESOURCE_NAME&_filter=entityTypes=$CUSTOM_PROPERTY_RESOURCE_TYPE&_field=uniqueId&_include_global_resources=false&_include_count=false&_include_status=false&_include_status_severity=false&_include_metadata=false&_return_composites=true" --insecure -u $LOGIN -H 'Content-Type: application/json' -H 'X-TenantID: cfd95b7e-3bc7-4006-a4a8-a73a79c71255'|jq -r -c '._items[]|._id'| tail -1)
         echo "----------------------------------------------------------------------------------------------------------"
         echo "     🌶️ Update Properties for $CUSTOM_PROPERTY_RESOURCE_NAME : $CUSTOM_PROPERTY_RESOURCE_TYPE : $OBJ_ID"
         export result=$(curl -k -s -X "POST" "$TOPO_MGT_ROUTE/1.0/topology/resources/$OBJ_ID" --insecure -u $LOGIN -H 'Content-Type: application/json' -H 'X-TenantID: cfd95b7e-3bc7-4006-a4a8-a73a79c71255' -d "{\"timestamp\": \"$my_timestamp\",\"Transactions per Second\": \"$RANDOM_TPS\",\"test\": \"$RANDOM_DELAY\"}")
