9699814c597fbb6798d183915b6732d0866d459d
Niklaus Hirt <nikh@ch.ibm.com>
2024-12-05 09:21:22 +0100

    Prep 4.8

--- tools/97_addons/ibm-aiops-tools-container/build.sh
+++ tools/97_addons/ibm-aiops-tools-container/build.sh
@@ -6,9 +6,9 @@ podman machine start
 podman login quay.io -u niklaushirt@gmail.com
 
 
-export CONT_VERSION=2.2
+export CONT_VERSION=2.3
 
-podman buildx build --platform linux/amd64 -t quay.io/niklaushirt/ibm-aiops-tools:$CONT_VERSION --load .
+podman buildx build --platform linux/amd64 -t quay.io/niklaushirt/ibm-aiops-tools:$CONT_VERSION --load -f=Containerfile
 podman push quay.io/niklaushirt/ibm-aiops-tools:$CONT_VERSION
 
 
@@ -20,5 +20,5 @@ export CONT_VERSION=2.0
 docker buildx build --platform linux/amd64 -t quay.io/niklaushirt/ibm-aiops-tools:$CONT_VERSION --load .
 docker push quay.io/niklaushirt/ibm-aiops-tools:$CONT_VERSION
 
-podman tag quay.io/niklaushirt/ibm-aiops-tools:2.2 quay.io/niklaushirt/ibm-aiops-tools:2.2
-podman push quay.io/niklaushirt/ibm-aiops-tools:2.2
+podman tag quay.io/niklaushirt/ibm-aiops-tools:2.3 quay.io/niklaushirt/ibm-aiops-tools:2.3
+podman push quay.io/niklaushirt/ibm-aiops-tools:2.3
---
+++ tools/97_addons/use-customer-data/generate-synthetic-data/generate-demo-data.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+
+clear
+echo "***************************************************************************************************************************************************"
+echo "***************************************************************************************************************************************************"
+echo "***************************************************************************************************************************************************"
+echo "***************************************************************************************************************************************************"
+echo ""
+echo "         ________  __  ___     ___    ________       "     
+echo "        /  _/ __ )/  |/  /    /   |  /  _/ __ \____  _____"
+echo "        / // __  / /|_/ /    / /| |  / // / / / __ \/ ___/"
+echo "      _/ // /_/ / /  / /    / ___ |_/ // /_/ / /_/ (__  ) "
+echo "     /___/_____/_/  /_/    /_/  |_/___/\____/ .___/____/  "
+echo "                                           /_/            "
+echo ""
+echo "***************************************************************************************************************************************************"
+echo "***************************************************************************************************************************************************"
+echo ""
+echo " 🚀  IBMAIOPS - Create Synthetic Metrics Data"
+echo ""
+echo "***************************************************************************************************************************************************"
+echo "***************************************************************************************************************************************************"
+echo "***************************************************************************************************************************************************"
+
+
+# Output Directory
+export OUTPUT_DIR=/tmp/synthetic-data/
+
+# Arbitrary name of your metric data
+export DATA_NAME=demo
+
+# Number of Metric Datapoints
+export NUMBER_DATAPOINTS=100
+
+# Metric Format:
+#   resource_name, metric_name, group_name, fixed base value, random value
+#
+#   Example for "fixed base value"=100 and 2random value"=10
+#   Gives metrics values between 100 and 110
+#   If you put "fixed base value"=ITERATIONS then it will use the counter (for PodRestarts for example)
+declare -a MY_RES_IDS=(
+  "demo-metric,DemoUsage1,DemoGroup,0,10000"
+  "demo-metric,DemoUsage2,DemoGroup,0,10000"
+  "demo-metric,DemoUsage3,DemoGroup,0,10000"
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# DO NOT MODIFY BELOW
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# Number of Global Iterations
+export TIME_INCREMENT_MINUTES="0" 
+export TIME_INCREMENT_SECONDS="10" 
+export ADD_MSECONDS_STRING=000
+export DATE_FORMAT="+%s"
+export DATE_FORMAT_READABLE="+%Y-%m-%d %H:%M:%S"
+export METRICS_SKEW="-13M"
+export ADD_SECONDS=120
+
+# Create Output Filename
+export file_timestamp=$(date "+%Y-%m-%d-%H-%M-%S")
+mkdir -p $OUTPUT_DIR
+export FILENAME=$OUTPUT_DIR"synthetic-data-"$DATA_NAME"-"$file_timestamp".json"
+
+
+echo "   "
+echo "   "
+echo "   "
+echo "   "
+echo "   ----------------------------------------------------------------------------------------------------------------------------------------"
+echo "     🚀  Generate Synthetic Data for $DATA_NAME in $FILENAME"
+echo "   ----------------------------------------------------------------------------------------------------------------------------------------"
+echo "   "
+echo "   "
+
+
+
+for value in "${MY_RES_IDS[@]}"
+do
+      MY_RESOURCE_ID=$(echo $value | cut -d',' -f1)
+      MY_METRIC_NAME=$(echo $value | cut -d',' -f2)
+      MY_GROUP_ID=$(echo $value | cut -d',' -f3)
+      MY_FIX_VALUE=$(echo $value | cut -d',' -f4)
+      #echo ":"$MY_FIX_VALUE
+      if [[ $MY_FIX_VALUE == "ITERATIONS" ]]; then
+            MY_FIX_VALUE=$(( BUNDLE_ITERATIONS*NUMBER_DATAPOINTS+ITERATIONS ))
+      fi
+      MY_VARIATION=$(echo $value | cut -d',' -f5)
+
+      echo "     🚀  Generate Synthetic Data for $MY_METRIC_NAME"
+      echo "   "
+
+      ADD_SECONDS=0
+      MY_TIMESTAMP=$(date -v "$METRICS_SKEW" -v "+"$ADD_SECONDS"S" "$DATE_FORMAT")"$ADD_MSECONDS_STRING"
+
+
+
+      for (( BUNDLE_ITERATIONS=1; BUNDLE_ITERATIONS <= $NUMBER_DATAPOINTS; ++BUNDLE_ITERATIONS ))
+      do
+
+            act_timestamp_begin=$(date -v "$METRICS_SKEW" -v "+"$ADD_SECONDS"S" "$DATE_FORMAT_READABLE")
+            ADD_SECONDS=$(($ADD_SECONDS+($TIME_INCREMENT_MINUTES*60)))
+
+            echo "             ♻️  ITERATION for $MY_METRIC_NAME: $NUMBER_DATAPOINTS-$BUNDLE_ITERATIONS   -     Seconds skew "$ADD_SECONDS"   - "$MY_TIMESTAMP
+
+            # Time Skew
+            ADD_SECONDS=$(($ADD_SECONDS+$TIME_INCREMENT_SECONDS))
+
+            # Get timestamp in ELK format
+            MY_TIMESTAMP=$(date -v "$METRICS_SKEW" -v "+"$ADD_SECONDS"S" "$DATE_FORMAT")"$ADD_MSECONDS_STRING"
+            CURRENT_VALUE=$(($RANDOM%$MY_VARIATION+$MY_FIX_VALUE))
+
+            echo "{\"timestamp\":\"$MY_TIMESTAMP\",\"resourceID\":\"$MY_RESOURCE_ID\",\"metrics\":{\"$MY_METRIC_NAME\":$CURRENT_VALUE},\"attributes\":{\"group\":\"$MY_GROUP_ID\",\"node\":\"$MY_RESOURCE_ID\"}},">> $FILENAME
+      done
+
+      act_timestamp_end=$(date -v "$METRICS_SKEW" -v "+"$ADD_SECONDS"S" "$DATE_FORMAT_READABLE")
+      echo "   "
+      echo "         ✅ $NUMBER_DATAPOINTS Datapoints created for Metric $MY_METRIC_NAME: From $act_timestamp_begin to $act_timestamp_end"
+      echo "   "
+      echo "   "
+      echo "   "
+      echo "   "
+
+done
+
+
+echo "   "
+echo "   "
+echo "   "
+echo "   "
+echo "--------------------------------------------------------------------------------------------------------------------------------"
+echo " ✅ DONE... You're good to go...."
+echo "--------------------------------------------------------------------------------------------------------------------------------"
+
+
+# cp /tmp/
