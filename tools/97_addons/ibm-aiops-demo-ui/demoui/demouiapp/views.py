0a79aa1a4d3903659c636199faf2f8ac08d14aa8
Niklaus Hirt <nikh@ch.ibm.com>
2025-02-04 14:58:38 +0100

    DemoUI 4.8.0

--- tools/97_addons/ibm-aiops-demo-ui/demoui/demouiapp/views.py
+++ tools/97_addons/ibm-aiops-demo-ui/demoui/demouiapp/views.py
@@ -44,7 +44,22 @@ print ('------------------------------------------------------------------------
 print ('')
 
 
+# from opentelemetry import trace
+# from opentelemetry.sdk.trace import TracerProvider
+# from opentelemetry.sdk.trace.export import (
+#     BatchSpanProcessor,
+#     ConsoleSpanExporter,
+# )
 
+# provider = TracerProvider()
+# processor = BatchSpanProcessor(ConsoleSpanExporter())
+# provider.add_span_processor(processor)
+
+# # Sets the global default tracer provider
+# trace.set_tracer_provider(provider)
+
+# # Creates a tracer from the global tracer provider
+# tracer = trace.get_tracer("my.tracer.name")
 
 
 
--- tools/97_addons/ibm-aiops-demo-ui/demoui/manage.py
+++ tools/97_addons/ibm-aiops-demo-ui/demoui/manage.py
@@ -3,10 +3,18 @@
 import os
 import sys
 
+# from opentelemetry.instrumentation.django import DjangoInstrumentor
+
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demoui.settings')
+
+
+    # DjangoInstrumentor().instrument(is_sql_commentor_enabled=True)
+
+
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
