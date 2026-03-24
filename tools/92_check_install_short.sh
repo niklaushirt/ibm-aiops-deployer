3e095c38435f69b9fb7a06b6dc41567848c41367
niklaushirt <niklaushirt@gmail.com>
2026-03-24 16:38:29 +0100

    Check install

--- tools/92_check_install_short.sh
+++ tools/92_check_install_short.sh
@@ -50,7 +50,7 @@ EOF
 # ---------------------------------------------------------------------------------------------------------------------------------------------------"
 # ---------------------------------------------------------------------------------------------------------------------------------------------------"
 function handleError(){
-    if  ([[ $CURRENT_ERROR|bool == true ]]); 
+    if  ([[ $CURRENT_ERROR == true ]]); 
     then
         ERROR=true
         ERROR_STRING=$ERROR_STRING"\n⭕ $CURRENT_ERROR_STRING"
@@ -72,7 +72,7 @@ function handleError(){
 
 
 function handleWarning(){
-    if  ([[ $CURRENT_WARNING_STATE|bool == true ]]); 
+    if  ([[ $CURRENT_WARNING_STATE == true ]]); 
     then
         WARNING_STATE=true
         WARNING_STRING=$WARNING_STRING"\n ⚠️  $CURRENT_WARNING_STRING"
@@ -676,7 +676,7 @@ EOF
 
       echo ""
       echo ""
-    if  ([[ $ERROR|bool == true ]]); 
+    if  ([[ $ERROR == true ]]); 
     then
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # ERROR
@@ -740,7 +740,7 @@ spec:
     text: "⚠️ IBMAIOPS is installed in this cluster. 🚀 Access the DemoUI with Password '$DEMO_PWD' here:"
 EOF
 
-    elif  ([[ $WARNING_STATE|bool == true ]]); 
+    elif  ([[ $WARNING_STATE == true ]]); 
     then
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # ERROR
