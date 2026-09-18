8104078ec2d8c945f3f4e3b59222372f89c53067
niklaushirt <niklaushirt@gmail.com>
2026-09-18 16:36:18 +0200

    Check PR

---
+++ tools/97_addons/checkwatsonxkey.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# checkwatsonxkey.sh
+#
+# Purpose: Check whether a given API Key exists. If found, show associated data to identify project IDs
+#
+# Author Markus W. Fehr
+# September 17, 2026
+
+if [ $# -eq 0 ]
+  then
+    echo ""
+    echo "No arguments supplied"
+    echo "Usage: $0 <API key>"
+    echo ""
+    exit
+fi
+
+API_KEY=$1
+
+echo ""
+echo "retrieval for API Key $API_KEY"
+echo ""
+
+RESPONSE=$(curl -s -X POST \
+https://iam.cloud.ibm.com/identity/token \
+-H "Content-Type: application/x-www-form-urlencoded" \
+-d "grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=$API_KEY")
+
+echo "$RESPONSE" | jq
+
+if echo "$RESPONSE" | jq -e '.errorMessage' > /dev/null 2>&1; then
+  echo ""
+  echo "Error returned from IAM. Exiting."
+  echo ""
+  exit 1
+fi
+
+read -p "Press enter to continue retrieving associated project IDs"
+
+IAM_TOKEN=$(echo "$RESPONSE" | jq -r .access_token)
+
+curl -s \
+-H "Authorization: Bearer $IAM_TOKEN" \
+"https://api.dataplatform.cloud.ibm.com/v2/projects?limit=100" | jq -r
+
+
